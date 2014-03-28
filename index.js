be11ceb907db2afe5dbdd84f716a8f6f3b2cc2ac
deathcap <deathcap@gmx.co.uk>
2014-03-28 09:03:39 -0700

    List inventory

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ var data = fs.readFileSync('test.dat');
 //var data = fs.readFileSync('../nbt-js/sample/bigtest.nbt');
 nbt.parse(data, function(err, result) {
   console.log(JSON.stringify(result,null, '  '));
+
+  if (result.Inventory) {
+    for (var i = 0; i < result.Inventory.length; i += 1) {
+      console.log(i, result.Inventory[i]);
+    }
+  }
+  // TODO: convert
 });
