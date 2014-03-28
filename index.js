c72e6820ad7696f5d8ad95a6d7e994d1a3fc8f1a
deathcap <deathcap@gmx.co.uk>
2014-03-28 09:06:15 -0700

    Convert counts to ItemPile

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 var nbt = require('nbt');
 var fs = require('fs');
+var ItemPile = require('itempile');
+
+var id2name = {
+  // TODO
+  default: 'gravel'
+};
 
 var data = fs.readFileSync('test.dat');
 //var data = fs.readFileSync('../nbt-js/sample/bigtest.nbt');
@@ -11,7 +17,12 @@ nbt.parse(data, function(err, result) {
   if (result.Inventory) {
     for (var i = 0; i < result.Inventory.length; i += 1) {
       console.log(i, result.Inventory[i]);
+
+      var name = id2name[result.Inventory[i].id] || id2name.default;
+      var count = +result.Inventory[i].Count;
+      var pile = new ItemPile(name, count);
+
+      console.log(pile);
     }
   }
-  // TODO: convert
 });
