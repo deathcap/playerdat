e28b669e8542d98b0b54666e68911d6d62fa14bd
deathcap <deathcap@gmx.co.uk>
2014-03-27 21:41:19 -0700

    Change to readFileSync

--- index.js
+++ index.js
@@ -3,11 +3,8 @@
 var nbt = require('nbt');
 var fs = require('fs');
 
-fs.readFile('test.dat', function(err, data) {
-//fs.readFile('../nbt-js/sample/bigtest.nbt', function(err, data) {
-  if (err) throw err;
-
-  nbt.parse(data, function(err, result) {
-    console.log(JSON.stringify(result,null, '  '));
-  });
+var data = fs.readFileSync('test.dat');
+//var data = fs.readFileSync('../nbt-js/sample/bigtest.nbt');
+nbt.parse(data, function(err, result) {
+  console.log(JSON.stringify(result,null, '  '));
 });
